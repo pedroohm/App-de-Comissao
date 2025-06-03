cf7f3c83df188cb5c32830fc6e8cd38d654e27eb
ojuanfreire <ojuansfr9@gmail.com>
2025-06-03 15:48:13 -0300

    Created User class

---
+++ app/src/main/java/com/grupo6/appdecomissao/User.java
@@ -0,0 +1,36 @@
+package com.grupo6.appdecomissao;
+
+public class User {
+    private String id;
+    private String name;
+    private String email;
+
+    // Possíveis atributos (picture e password) que deverão ser armazenados em um BD
+    // Independem da API
+    private String picture; // É o caminho relativo da imagem
+    private String password; // É um hash
+
+    // Atributo usado para identificar se o usuário é um consultor ou um supervisor
+    private String profile;
+
+    public User(String id, String name, String email, String picture, String password, String profile) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.password = password;
+        this.profile = profile;
+    }
+
+    // Métodos de Get
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+    public String getPicture() { return picture; }
+    public String getPassword() { return password; }
+    public String getProfile() { return profile; }
+
+    // Métodos de Set para os atributos que serão mutáveis
+    public void setName(String name) { this.name = name; }
+    public void setPicture(String picture) { this.picture = picture; }
+}

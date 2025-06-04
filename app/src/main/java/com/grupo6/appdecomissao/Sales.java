2ea3907bbc410b4f6828b61d25076446e6342a68
Pedro Henrique Barbosa Carvalho <pedrohbc1504@gmail.com>
2025-06-03 21:31:48 -0300

    Adding Sales class

---
+++ app/src/main/java/com/grupo6/appdecomissao/Sales.java
@@ -0,0 +1,30 @@
+package com.grupo6.appdecomissao;
+
+public class Sales {
+
+    private Integer id;
+    private Integer consultantId;
+    private String product;
+    private double price;
+    private String saleDate;
+    private double commission;
+    private Integer recordId;
+
+    public Sales(Integer id, Integer consultantId, String product, double price, String saleDate, double commission, Integer recordId){
+        this.id = id;
+        this.consultantId = consultantId;
+        this.product = product;
+        this.price = price;
+        this.saleDate = saleDate;
+        this.commission = commission;
+        this.recordId = recordId;
+    }
+
+    public Integer getId() { return id; }
+    public Integer getConsultantId() { return consultantId; }
+    public String getProduct() { return product; }
+    public double getPrice() { return price; }
+    public String getSaleDate() { return saleDate; }
+    public double getCommission() { return commission; }
+    public Integer getRecordId() { return recordId; }
+}

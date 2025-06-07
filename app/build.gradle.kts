8b5640f5d0771e9a189ddae3b2a90a23ba2bf074
Pedro Henrique Barbosa Carvalho <pedrohbc1504@gmail.com>
2025-06-07 16:59:17 -0300

    Update on gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,4 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }

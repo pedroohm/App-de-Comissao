261dbe4b7d996cf8280ec1a980c2b8852ebe575b
Pedro Moura <pedroh.moura37@gmail.com>
2025-06-08 17:57:10 -0300

    Link between screens and backend

--- app/src/main/java/com/grupo6/appdecomissao/activity/ManangerProfileConsultant.java
+++ app/src/main/java/com/grupo6/appdecomissao/activity/ManangerProfileConsultant.java
@@ -1,4 +1,4 @@
-package com.grupo6.appdecomissao;
+package com.grupo6.appdecomissao.activity;
 
 import android.os.Bundle;
 
@@ -8,13 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class DashboardConsultant extends AppCompatActivity {
+import com.grupo6.appdecomissao.R;
+
+public class ManangerProfileConsultant extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_dashboard_consultant);
+        setContentView(R.layout.activity_mananger_profile_consultant);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
